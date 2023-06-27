b6109b38d5931da32fd71917a9fd793bae383772
Denis Murashov <mdenis97@gmail.com>
2023-06-27 11:15:32 +0300

    third commit

--- entities/File.php
+++ entities/File.php
@@ -1,7 +1,7 @@
 <?php
 
 class File {
-    private $root = 'C:/php/xampp/htdocs/userFiles';
+    private $root = 'C:/php/xampp/htdocs/fileStorage';
 
 // Создание файла
     public static function create($obj)
@@ -60,83 +60,184 @@ class File {
 // Перемещение или переименование файла
     public static function update($obj, $id)
     {
-        //parse_str(file_get_contents("php://input"), $PUT);
-        //print_r($PUT);
-        //if (isset($PUT['newFileName'])) {
-        //    $searchResult = [];
-        //    $obj->findFiles($obj->root, $searchResult);
-        //    if ($searchResult === array()) {
-        //        echo 'Нет сохраненных файлов.';
-        //    } else {
-        //        $success = -1;
-        //        foreach ($searchResult as $key => $fileName) {
-        //            if (str_starts_with($fileName, $id)) {
-        //                $success = $key;
-        //            }
-        //        }
-        //        if ($success > -1) {
-        //            rename("$obj->root/" . $searchResult[$success], "$obj->root/" . "$id " . $PUT['newFileName']);
-        //        } else {
-        //            echo 'Такого файла нет в хранилище.';
-        //        }
-        //    }
-        //}
-        //if (isset($PUT['newFileRoot'])) {
-        //    $searchResult = [];
-        //    $obj->findFiles($obj->root, $searchResult);
-        //    if ($searchResult === array()) {
-        //        echo 'Нет сохраненных файлов.';
-        //    } else {
-        //        $success = 0;
-        //        foreach ($searchResult as $fileName) {
-        //            if (str_starts_with($fileName, $id)) {
-        //                $success = 1;
-        //            }
-        //        }
-        //        if ($success == 1) {
-        //            echo "$obj->root/" . $PUT['newFileRoot'] . "/$fileName";
-        ////            rename("$obj->root/$fileName", "$obj->root/" . $PUT['newFileRoot'] . "/$fileName");
-        //        } else {
-        //            echo 'Такого файла нет в хранилище.';
-        //        }
-        //    }
-        //}
+        parse_str(file_get_contents("php://input"), $PUT);
+        if (isset($PUT['newFileName'])) {
+            $searchResult = [];
+            $obj->findFiles($obj->root, $searchResult);
+            if ($searchResult === array()) {
+                echo 'Нет сохраненных файлов.';
+            } else {
+                $success = -1;
+                foreach ($searchResult as $key => $fileName) {
+                    if (str_starts_with($fileName, $id)) {
+                        $success = $key;
+                    }
+                }
+                if ($success > -1) {
+                    rename($searchResult[$success+1] . $searchResult[$success], $searchResult[$success+1] . "$id " . $PUT['newFileName']);
+                } else {
+                    echo 'Такого файла нет в хранилище.';
+                }
+            }
+        }
+        if (isset($PUT['newFileRoot'])) {
+            $searchResult = [];
+            $obj->findFiles($obj->root, $searchResult);
+            if ($searchResult === array()) {
+                echo 'Нет сохраненных файлов.';
+            } else {
+                $success = -1;
+                foreach ($searchResult as $key => $fileName) {
+                    if (str_starts_with($fileName, $id)) {
+                        $success = $key;
+                    }
+                }
+                if ($success > -1) {
+                    rename($searchResult[$success+1] . $searchResult[$success], "$obj->root/" . $PUT['newFileRoot'] . "/$searchResult[$success]");
+                } else {
+                    echo 'Такого файла нет в хранилище.';
+                }
+            }
+        }
     }
 
 // Удаление файла
     public static function delete($obj, $id)
     {
-        ;
+        $searchResult = [];
+        $obj->findFiles($obj->root, $searchResult);
+        if ($searchResult === array()) {
+            echo 'Нет сохраненных файлов.';
+        } else {
+            $success = -1;
+            foreach ($searchResult as $key => $fileName) {
+                if (str_starts_with($fileName, $id)) {
+                    $success = $key;
+                }
+            }
+            if ($success > -1) {
+                unlink($searchResult[$success+1] . $searchResult[$success]);
+                echo 'Файл успешно удален!';
+            } else {
+                echo 'Такого файла нет в хранилище.';
+            }
+        }
     }
 
 // Создание директории
     public static function createFolder($obj)
     {
-        ;
+        $id = 1;
+        $searchResult = [];
+        $obj->findDirs($obj->root, $searchResult);
+        if ($searchResult === array()) {
+            mkdir("$obj->root/" . $_POST['newFolder'] . "_$id");
+        } else {
+            foreach ($searchResult as $dirName) {
+                for (; str_ends_with($dirName, $id); $id++);
+            }
+            print_r($searchResult);
+            mkdir("$obj->root/" . $_POST['newFolder'] . "_$id");
+        }
     }
 
 // Получение списка файлов директории
     public static function readFolder($obj, $id)
     {
-        ;
+        $searchResult = [];
+        $obj->findDirs($obj->root, $searchResult);
+        if ($searchResult === array()) {
+            echo 'Отсутствуют директории в хранилище.';
+        } else {
+            $success = -1;
+            foreach ($searchResult as $key => $dirName) {
+                if (str_ends_with($dirName, $id)) {
+                    $success = $key;
+                }
+            }
+            if ($success > -1) {
+                $newSearchResult = [];
+                $obj->findFiles("$obj->root/$searchResult[$success]", $newSearchResult);
+                if ($newSearchResult === array()) {
+                    echo 'Нет сохраненных файлов.';
+                } else {
+                    print_r($newSearchResult);
+                }
+            } else {
+                echo 'Такой директории нет в хранилище.';
+            }
+        }
     }
 
 // Переименование директории
     public static function updateFolder($obj, $id)
     {
-        ;
+        parse_str(file_get_contents("php://input"), $PUT);
+        if (isset($PUT['newFolderName'])) {
+            $searchResult = [];
+            $obj->findDirs($obj->root, $searchResult);
+            if ($searchResult === array()) {
+                echo 'Отсутствуют директории в хранилище.';
+            } else {
+                $success = -1;
+                foreach ($searchResult as $key => $dirName) {
+                    if (str_ends_with($dirName, $id)) {
+                        $success = $key;
+                    }
+                }
+                if ($success > -1) {
+                    rename("$obj->root/$searchResult[$success]", "$obj->root/" . $PUT['newFolderName'] . "_$id");
+                } else {
+                    echo 'Такой директории нет в хранилище.';
+                }
+            }
+        }
     }
 
 // Удаление директории
     public static function deleteFolder($obj, $id)
     {
-        ;
+        $searchResult = [];
+        $obj->findDirs($obj->root, $searchResult);
+        if ($searchResult === array()) {
+            echo 'Отсутствуют директории в хранилище.';
+        } else {
+            $success = -1;
+            foreach ($searchResult as $key => $dirName) {
+                if (str_ends_with($dirName, $id)) {
+                    $success = $key;
+                }
+            }
+            if ($success > -1) {
+                rmdir("$obj->root/$searchResult[$success]");
+            } else {
+                echo 'Такой директории нет в хранилище.';
+            }
+        }
     }
 
 // Получение списка пользователей, имеющих доступ к файлу
     public static function readUsers($obj, $id)
     {
-        ;
+        $searchResult = [];
+        $obj->findFiles($obj->root, $searchResult);
+        if ($searchResult === array()) {
+            echo 'Нет сохраненных файлов.';
+        } else {
+            $success = -1;
+            foreach ($searchResult as $key => $fileName) {
+                if (str_starts_with($fileName, $id)) {
+                    $success = $key;
+                }
+            }
+            if ($success > -1) {
+                session_start();
+                chown($searchResult[$success+1] . $searchResult[$success], $_SESSION['id']);
+                print_r(posix_getpwuid(stat($searchResult[$success+1] . $searchResult[$success])['uid']));
+            } else {
+                echo 'Такого файла нет в хранилище.';
+            }
+        }
     }
 
 // Добавление доступа к файлу для пользователя
@@ -160,9 +261,24 @@ class File {
             }
             elseif (is_file("$root/$value")) {
                 $searchResult[] = "$value";
+                $searchResult[] = "$root/";
             }
             elseif (is_dir("$root/$value")) {
                 $this->findFiles("$root/$value", $searchResult);
+            }
+        }
+    }
+
+// Рекурсивный поиск папок в хранилище
+    private function findDirs($root, &$searchResult)
+    {
+        foreach (scandir($root)  as $value) {
+            if ($value == '.' || $value == '..') {
+                continue;
+            }
+            elseif (is_dir("$root/$value")) {
+                $searchResult[] = "$value";
+                $this->findDirs("$root/$value", $searchResult);
             }
         }
     }
